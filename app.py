3baaec73fc0a3d8ddbd98c338c75ddbc4bc3e014
JeveCasarin <156836775+JeveCasarin@users.noreply.github.com>
2026-04-01 22:22:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/spreadsheets"
 ]
 
 def conectar_planilha():
